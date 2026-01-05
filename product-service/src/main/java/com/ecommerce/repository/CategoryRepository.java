b193b4e85df27c0d513e11e7ed24ee60588985f7
numerouno_life <dollari.91@mail.ru>
2026-01-05 11:58:08 +0400

    написал тесты CategoryRepositoryTest.java

--- product-service/src/main/java/com/ecommerce/repository/CategoryRepository.java
+++ product-service/src/main/java/com/ecommerce/repository/CategoryRepository.java
@@ -3,9 +3,11 @@ package com.ecommerce.repository;
 import com.ecommerce.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     boolean existsByName(String name);
 
-    Category findByName(String name);
+    Optional<Category> findByName(String name);
 }
