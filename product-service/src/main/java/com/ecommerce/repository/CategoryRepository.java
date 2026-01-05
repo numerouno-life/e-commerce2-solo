1be580162bfc107006b67b74399ae75346ab259d
numerouno_life <dollari.91@mail.ru>
2026-01-05 11:33:50 +0400

    создал CategoryRepository

---
+++ product-service/src/main/java/com/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByName(String name);
+
+    Category findByName(String name);
+}
